5a468e4d9ea438e73ff5873e939d31958eafeab8
Ranjitha <Ranjitha-Linux@Ranjitha.localdomain>
2026-03-18 09:45:42 +0000

    added vpc networking terraform module

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,70 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+  project                 = var.project_id
+  description             = "Custom VPC for Employee API project"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = var.subnet_name
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  project       = var.project_id
+  private_ip_google_access = true
+  description = "Primary subnet for Employee API resources"
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  name          = "${var.vpc_name}-private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.id
+  project       = var.project_id
+  description = "Private IP range for Cloud SQL private service connection"
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "${var.vpc_name}-allow-internal"
+  network = google_compute_network.vpc.id
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = [var.subnet_cidr]
+  description   = "Allow internal communication within the VPC"
+}
+
+resource "google_compute_firewall" "allow_health_checks" {
+  name    = "${var.vpc_name}-allow-health-checks"
+  network = google_compute_network.vpc.name
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.232.0.0/16"]
+  description   = "Allow Google cloud health check"
+  
+
